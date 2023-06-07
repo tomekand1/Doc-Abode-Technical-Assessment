5532a2e94f0acd2bc48341f86e5a683e0671f976
tomekand1 <tomekand1@gmail.com>
2023-06-07 19:56:34 +0100

    updated database model with MongoMemoryServer

--- src/models/model.js
+++ src/models/model.js
@@ -1,15 +1,27 @@
 const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const { v4: uuidv4 } = require("uuid");
+const { omit } = require("lodash");
+
+/* c8 ignore start */
+(async () => {
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+  await mongoose.connect(mongoUri);
+})();
+
+const Schema = mongoose.Schema;
 
 const sanitizeOutput = (_doc, obj) => {
   return omit(obj, ["__v", "_id"]);
 };
+/*c8 ignore end */
+
 const options = {
   toJSON: {
     transform: sanitizeOutput,
   },
 };
-const Schema = mongoose.Schema;
-
 let model = new Schema(
   {
     id: {
