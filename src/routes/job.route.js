cb632e9583893f244dd9d6841b72e30810791a2c
tomekand1 <tomekand1@gmail.com>
2023-06-07 19:08:47 +0100

    added post job handler to route composition

--- src/routes/job.route.js
+++ src/routes/job.route.js
@@ -1,9 +1,14 @@
-const { getJobs } = require("../controllers/job.controller");
+const { getJobs, postJob } = require("../controllers/job.controller");
 
 module.exports = [
   {
     method: "GET",
     path: "/jobs",
     handler: getJobs,
+  },
+  {
+    method: "POST",
+    path: "/jobs",
+    handler: postJob,
   },
 ];
